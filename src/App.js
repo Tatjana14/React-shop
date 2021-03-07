aa89ef5cfe0c0cce30cae64a9b1513f5e90c914b
danshkk <shkurkodan@gmail.com>
2021-03-07 15:47:37 +0300

    Add Header + Add common folder in img

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './header/Header'
 import Footer from './footer/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Footer />
     </div>
   );
---
+++ src/header/headerMenu/HeaderMenu.js
@@ -0,0 +1,21 @@
+
+import './HeaderMenu.css';
+
+function HeaderMenu() {
+  return (
+    <div className="HeaderMenu">
+      <ul className="HeaderMenuItems">
+        <li><a href="">Квадроциклы</a></li>
+        <li><a href="">Катера</a></li>
+        <li><a href="">Гидроциклы</a></li>
+        <li><a href="">Лодки</a></li>
+        <li><a href="">Вездеходы</a></li>
+        <li><a href="">Снегоходы</a></li>
+        <li><a href="">Двигатели</a></li>
+        <li><a href="">Запчасти</a></li>
+      </ul>
+    </div>
+  );
+}
+
+export default HeaderMenu;
