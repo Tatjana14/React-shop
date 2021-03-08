c8dbb5282f3c786264162428fec51dd07b3f95a2
danshkk <shkurkodan@gmail.com>
2021-03-08 13:34:37 +0300

    add links to header and images

--- src/header/headerNav/HeaderNav.js
+++ src/header/headerNav/HeaderNav.js
@@ -4,25 +4,27 @@ import map from "../../assets/img/header/map.svg";
 import like from "../../assets/img/common/like.svg";
 import user from "../../assets/img/header/user.svg";
 import count from "../../assets/img/header/count.svg";
+import Image from '../../components/Image/Image';
+import Link from '../../components/Link/Link';
 
 
 function HeaderNav() {
   return (
     <div className="HeaderNav">
       <ul className="HeaderNavItems">
-        <li><a href="">Магазины</a></li>
-        <li><a href="">Акции</a></li>
-        <li><a href="">Доставка и оплата</a></li>
+        <li><Link linkName="Магазины"/></li>
+        <li><Link linkName="Акции"/></li>
+        <li><Link linkName="Доставка и оплата"/></li>
       </ul>
-      <img src={logo} alt=""/>
+      <Image src={logo} alt="#"/>
       <div className="HeaderMap">
-        <img src={map} alt=""/>
+        <Image src={map} alt="#"/>
         <span>Москва, ул. Науки 25</span>
       </div>
       <div className="HeaderPersonal">
-        <a href=""><img src={like} alt=""/></a>
-        <a href=""><img src={user} alt=""/></a>
-        <a href=""><img src={count} alt=""/></a>
+        <a href=""><Image src={like} alt="#"/></a>
+        <a href=""><Image src={user} alt="#"/></a>
+        <a href=""><Image src={count} alt="#"/></a>
       </div>
     </div>
   );
