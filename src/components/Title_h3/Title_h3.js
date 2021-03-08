31fd91010c6bf4e7ef07f3fcd5ffd6d0138b9b82
borislav12322 <76974544+borislav12322@users.noreply.github.com>
2021-03-08 15:34:59 +0300

    Add component "title_h3"

---
+++ src/components/Title_h3/Title_h3.js
@@ -0,0 +1,10 @@
+import './Title_h3.css';
+
+function Title_h3(props){
+    return(
+        <h3 className='title-h3'>{props.title_h3Name}</h3>
+    );
+
+}
+
+export default Title_h3;
--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -5,6 +5,7 @@ import Link from '../components/Link/Link'
 import Menu_information from './Menu_information/Menu_information';
 import Menu_onlineRetail from './Menu_onlineRetail/Menu_onlineRetail';
 import ListSocial from './ListSocial/ListSocial';
+import Title_h3 from '../components/Title_h3/Title_h3';
 
 function Footer() {
   return (
@@ -12,7 +13,7 @@ function Footer() {
       <div className="footer-top">
         <div className="footer-subscribe">
           <div className="footer-subscribe__title">
-            Подпишитесь на нашу рассылку и узнавайте о акция быстрее
+            <Title_h3 title_h3Name="Подпишитесь на нашу рассылку и узнавайте о акция быстрее" />
           </div>
 
           <form className="footer-subscribe__email-form">
@@ -23,10 +24,12 @@ function Footer() {
 
 
         <div className="footer-top__information-list">
+        <Title_h3 title_h3Name="Информация" />
           <Menu_information />
         </div>
 
         <div className="footer-top__onlineRetail-list">
+        <Title_h3 title_h3Name="Интернет-магазин" />
           <Menu_onlineRetail />
         </div>
 
