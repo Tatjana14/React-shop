ea999543e822fe5c6da1f9bb4d1c8a9c6f660368
danshkk <shkurkodan@gmail.com>
2021-03-11 00:40:49 +0300

    n

--- src/App.js
+++ src/App.js
@@ -10,17 +10,10 @@ import Product1 from './pages/product/Product';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      {/* <Header/> */}
-      {/* <Main/> */}
-      <Catalog/>
-      {/* <Footer /> */}
-=======
       <Header/>
       <Main/>
       <Footer />
       <Product1 />
->>>>>>> 1a191ba4a1da6aa9f8687587fa0f69ea7fa5df31
     </div>
   );
 }
