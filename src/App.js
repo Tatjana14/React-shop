e00c18d3c96ad352df5c8b40027a5b1a12787c22
Tatjana14 <tanycha1403@gmail.com>
2021-03-13 13:16:30 +0300

    fonts + mixinFont + css for app

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ function App() {
             <Main/> 
             <Catalog/>
             <Product1 />
-            {/* <Footer /> */}
             </div>
+
       </div>
       );
 }
--- src/components/categoriesCard/CategoriesCard.js
+++ src/components/categoriesCard/CategoriesCard.js
@@ -3,12 +3,17 @@ import s from './CategoriesCard.module.scss';
 
 function CategoriesCard(props) {
   return (
-    <div className = {s.wrapper}>
-      <a className = {s.linkCategories} href= {props.categoriesAdress}><h3 className = {s.title}>{props.categoriesValue}</h3></a>
-      <a className = {s.linkMore} href={props.categoriesAdress2}>Подробнее</a>
-      <div className = {s.wrapperImg}>
-        <img className = {s.imgCategories} src={props.imgCategories} alt={props.altCategories}/>
+    <div className = {s.category}>
+      <div className = {s.wrapper} >
+        <div className = {s.links}>
+          <a className = {s.linkCategory} href= {props.categoriesAdress}><h3 className = {s.title}>{props.categoriesValue}</h3></a>
+          <a className = {s.linkMore} href={props.categoriesAdress2}>Подробнее &rsaquo;</a>
+        </div>
+        <div className = {s.image}>
+          <img className = {s.imgCategories} src={props.imgCategories} alt={props.altCategories}/>
+        </div>        
       </div>
+      
     </div>
     )
 }
