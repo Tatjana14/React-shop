a681a498703f04d30d3a8444c609c31a1b613a06
Tatjana14 <tanycha1403@gmail.com>
2021-03-13 18:28:54 +0300

    fonts + categories

--- src/components/categoriesCard/CategoriesCard.js
+++ src/components/categoriesCard/CategoriesCard.js
@@ -4,16 +4,13 @@ import s from './CategoriesCard.module.scss';
 function CategoriesCard(props) {
   return (
     <div className = {s.category}>
-      <div className = {s.wrapper} >
         <div className = {s.links}>
           <a className = {s.linkCategory} href= {props.categoriesAdress}><h3 className = {s.title}>{props.categoriesValue}</h3></a>
-          <a className = {s.linkMore} href={props.categoriesAdress2}>Подробнее &rsaquo;</a>
+          <a className = {s.linkMore} href={props.categoriesAdress2}>Подробнее </a>
         </div>
-        <div className = {s.image}>
-          <img className = {s.imgCategories} src={props.imgCategories} alt={props.altCategories}/>
-        </div>        
-      </div>
-      
+        <div style = {props.style} className = {s.image} >
+          <img  className = {s.imgCategories} src={props.imgCategories} alt={props.altCategories}/>
+        </div>              
     </div>
     )
 }
