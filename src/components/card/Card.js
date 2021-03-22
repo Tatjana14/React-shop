082a81296c28f91a9ebe973f92bebd45d1848749
Tatjana14 <tanycha1403@gmail.com>
2021-03-22 22:19:47 +0300

    hhh

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,7 +3,7 @@ import s from './Card.module.scss';
 import like from '../../assets/img/common/like.svg';
 import count from '../../assets/img/common/count_button.svg';
 import Sticker from '../sticker/Sticker';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 
 
@@ -14,8 +14,8 @@ function Card(props) {
     return (
         <div style={props.styleCard} className={s.card}>
             <div className={s.actionCard}>
-                <NavLink className={s.actionCardText} to={props.product} >посмотреть товар</NavLink>
-                </div>
+                <NavLink to={props.product} className={s.actionCardText} >посмотреть товар</NavLink>
+            </div>
                 <Sticker Sticker={props.Sticker} className={s.cardSticker} saleValue = {props.value}/>
                 <button className={s.btnLike}><img className={s.imgLike} src={like} alt=""/></button>
             <div className={s.cardLink} href={props.to}>
